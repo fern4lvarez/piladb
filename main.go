6e6892c17d4274583d3e180d96ce2ea078b201ee
Fernando Álvarez <info@fer.ac>
2015-09-09 13:33:32 +0200

    Make project root go-getable

---
+++ main.go
@@ -0,0 +1,9 @@
+// piladb is a RESTful database engine based on the stack data structure.
+// Create an ilimited number of stacks, which can contain any kind of supported data types.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("file generate to make project root go-getable.")
+}
