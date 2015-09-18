34e5c9e713d9d90dac4b2d19ef5c56ff3b38154e
Fernando Álvarez <info@fer.ac>
2015-09-19 01:18:34 +0200

    pilad: set custom port on main test

--- pilad/main_test.go
+++ pilad/main_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"os"
 	"testing"
 	"time"
 )
 
 // TestMain is a hack to get 100% test coverage.
 func TestMain(t *testing.T) {
+	os.Setenv("PILADB_PORT", "35343")
 	go main()
 	time.Sleep(5 * time.Millisecond)
+	os.Setenv("PILADB_PORT", "")
 }
