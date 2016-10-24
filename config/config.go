803be1732ec964caf7ca4e54dc72a45297812e24
Fernando Alvarez <info@fer.ac>
2016-10-24 21:29:24 +0200

    config: first implementation

---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"github.com/fern4lvarez/piladb/pila"
+	"github.com/fern4lvarez/piladb/pkg/uuid"
+)
+
+// CONFIG represents the name of the database that will hold
+// all the config values.
+const CONFIG = "_config"
+
+// Config represents a Database containing all
+// configuration values that will be
+// updated and consumed by piladb.
+type Config struct {
+	Values *pila.Database
+}
+
+// NewConfig creates a new Config with empty values.
+func NewConfig() *Config {
+	values := pila.NewDatabase(CONFIG)
+	return &Config{Values: values}
+}
+
+// Get gets a config value from a key.
+func (c *Config) Get(key string) interface{} {
+	s, ok := c.Values.Stacks[uuid.New(CONFIG+key)]
+	if !ok {
+		return nil
+	}
+	return s.Peek()
+}
+
+// Set sets a config value having a key and the value.
+func (c *Config) Set(key string, value interface{}) {
+	s, ok := c.Values.Stacks[uuid.New(CONFIG+key)]
+	if !ok {
+		sID := c.Values.CreateStack(key)
+		s, _ = c.Values.Stacks[sID]
+	}
+
+	s.Push(value)
+}
---
+++ config/config_test.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/fern4lvarez/piladb/pkg/uuid"
+)
+
+func TestNewConfig(t *testing.T) {
+	config := NewConfig()
+
+	if config.Values == nil {
+		t.Fatal(errors.New("Config is nil"))
+	}
+
+	inputOutput := []struct {
+		input  interface{}
+		output interface{}
+	}{
+		{input: config.Values.Name, output: CONFIG},
+		{input: config.Values.ID, output: uuid.New(CONFIG)},
+		{input: len(config.Values.Stacks), output: 0},
+	}
+
+	for _, io := range inputOutput {
+		if io.input != io.output {
+			t.Errorf("got %v, expected %v", io.input, io.output)
+		}
+	}
+}
+
+func TestConfigGet(t *testing.T) {
+	config := NewConfig()
+
+	stackID := config.Values.CreateStack("foo")
+	s, _ := config.Values.Stacks[stackID]
+	s.Push("bar")
+	expectedValue := s.Peek()
+
+	if value := config.Get("foo"); value != expectedValue {
+		t.Errorf("Values is %s, expected %s", value, expectedValue)
+	}
+
+	if value := config.Get("no-exist"); value != nil {
+		t.Errorf("Values is %s, expected nil", value)
+	}
+}
+
+func TestConfigSet(t *testing.T) {
+	config := NewConfig()
+	expectedValues := []string{"bar", "baz", "bam"}
+
+	for _, expectedValue := range expectedValues {
+		config.Set("foo", expectedValue)
+		s, _ := config.Values.Stacks[uuid.New(CONFIG+"foo")]
+		if value := s.Peek(); value != expectedValue {
+			t.Errorf("Values is %s, expected %s", value, expectedValue)
+		}
+	}
+}
